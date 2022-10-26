d7729ecef53518c3d98cb0ad61421afe03d6834a
Gornostaeva <kate.gornostaeva@inbox.ru>
2022-10-26 17:22:01 +0300

    mb end?

---
+++ site_feedback.php
@@ -0,0 +1,11 @@
+<?php
+$title="Feedback";
+$title="Cat breeds";
+
+$today= date("d.m.y");
+$time = date("G:i:s");
+?>
+
+<?php require("components/header.php")?>
+<?php require("components/feedback.php")?>
+<?php require("components/footer.php")?>
---
+++ site_login.php
@@ -0,0 +1,12 @@
+<?php
+$title="Authorization";
+$title="Cat breeds";
+
+$today= date("d.m.y");
+$time = date("G:i:s");
+?>
+
+
+<?php require("components/header.php")?>
+<?php require("components/login.php")?>
+<?php require("components/footer.php")?>
